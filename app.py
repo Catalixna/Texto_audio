d0aebfb684c6f81eb915838c8eff05ea9fb47b59
Catalixna <ccardenasaa@gmail.com>
2024-10-20 20:34:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ st.subheader( "Texto ejemplo: ")
 st.write('En un rincón oculto de la selva, vivían los unicornios de agua, criaturas mágicas con cuernos cristalinos que brillaban como las estrellas. Estos unicornios no galopaban, sino que se deslizaban por ríos y lagunas, purificando las aguas y devolviendo la vida a todo lo que tocaban. Solo aparecían ante quienes realmente protegían la naturaleza.')
            
 st.markdown(f"Para escucharlo, copia el texto!!")
-text = st.text_area("Ingrese El texto a escuchar.")
+text = st.text_area("Pon aqui el texto que quieres escuchar.")
 
 tld='com'
 option_lang = st.selectbox(
