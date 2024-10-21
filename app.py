4660aa4ef1da7796b94f273cf4e4d12402480c01
Catalixna <ccardenasaa@gmail.com>
2024-10-20 20:33:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ try:
 except:
     pass
 
-st.subheader(color: #5f7f7a; "Texto ejemplo: ")
+st.subheader( "Texto ejemplo: ")
 st.write('En un rincón oculto de la selva, vivían los unicornios de agua, criaturas mágicas con cuernos cristalinos que brillaban como las estrellas. Estos unicornios no galopaban, sino que se deslizaban por ríos y lagunas, purificando las aguas y devolviendo la vida a todo lo que tocaban. Solo aparecían ante quienes realmente protegían la naturaleza.')
            
 st.markdown(f"Para escucharlo, copia el texto!!")
